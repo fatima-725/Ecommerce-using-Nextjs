2e009552882a92ac071d27ff2846c461ca25f090
fatima <fatima.tz8490@gmail.com>
2024-05-27 16:28:31 +0500

    Product list page - successfully retrieving data from db

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    images: {
-      remotePatterns: [{ hostname: "images.unsplash.com" }],
-    },
-    serverActions: true,
+  // experimental: {
+  images: {
+    remotePatterns: [{ hostname: "images.unsplash.com" }],
   },
+  serverActions: true,
+  // },
 };
 
 export default nextConfig;
